030ca823418971b312aa0d418b341abea828daee
DANISH KHAN <123612696+Mompa786@users.noreply.github.com>
2023-08-22 20:03:15 +0530

    Main.jsx

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,35 +1,59 @@
-import React ,{useState,useEffect}from 'react';
-import PostList from '../PostList';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../App.css';
 
+const Card = ({ title, body }) => (
+  <div className="card">
+    <h3>{title}</h3>
+    <p>{body}</p>
+  </div>
+);
 
-function Main(){
-    const[SearchText,setSearchText] = useState('');
-    const [filteredPosts, setFilteredPosts]=useState([]);
-    const [allPosts, setAllPosts] = useState([]);
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-    useEffect(()=>{
-        fetch('http://jsonplaceholder.typicode.com/posts')
-        .then(response =>response.json())
-        .then(data =>{
-            setAllPosts(data);
-            setFilteredPosts(data);
-        });
-    } ,[])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setAllPosts(response.data);
+        setFilteredPosts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
 
+  const handleSearch = () => {
+    if (searchText.trim() === '') {
+      setFilteredPosts(allPosts);
+    } else {
+      const filtered = allPosts.filter(post =>
+        post.title.includes(searchText)
+      );
+      setFilteredPosts(filtered);
+    }
+  };
 
+  return (
+    <div className="App">
+      <div className="centered">
+        <input
+          type="text"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          placeholder="Enter search text"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="posts">
+        {filteredPosts.map(post => (
+          <Card key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    const handleSearch = ()=>{
-        const filtered = allPosts.filter(post =>
-           post.tile.toLowerCase().include(SearchText.toLowerCase()) );
-           setFilteredPosts(filtered);
-    };
-    return(
-
-        <div className='main'>
-            <input type="text" value={SearchText} onChange={e=>setSearchText(e.target.value)} placeholder='search text...' />
-            <button onClick={handleSearch} > Search</button>
-            <PostList posts= {filteredPosts}/>
-        </div>
-    )
-}
-export default Main;
+export default App;
